709ed8fded2f79c20e273f1fc72d8aecb82a247b
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2023-06-13 17:05:15 -0500

    feat:update-details

--- src/containers/pages/Blog.jsx
+++ src/containers/pages/Blog.jsx
@@ -5,11 +5,11 @@ const Blog = () => {
   return (
     <Layout>
       <div className="lg:py-10 space-y-12 bg-slate-100">
-        <h1 className="text-center text-black font-bold md:text-3xl text-2xl my-5">
+        <h1 className="text-center text-black font-circular-medium tracking-wider md:text-3xl text-2xl my-5">
           Nuestros blog de noticias
         </h1>
         <div className="flex justify-between lg:px-10 px-4 items-center">
-          <h1 className="text-black text-2xl font-bold md:text-3xl">
+          <h1 className="text-black text-xl font-circular-medium md:text-2xl">
             Publicaciones Recientes
           </h1>
           <button className="underline py-1.5 px-3 md:py-2 md:px-3 font-semibold text-black rounded-lg  hover:text-indigo-600">
