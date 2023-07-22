423b07e1dad62e1acc0b0a010f58e135b8cf84af
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2023-07-22 17:46:13 -0500

    feat:update

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,8 +2,6 @@ import Banner from "../components/home/Banner";
 import Clients from "../components/home/Clients";
 import Feature from "../components/home/Feature";
 import Promotions from "../components/home/Promotions";
-import Testimonials from "../components/home/Testimonials";
-import TestimonialsLToR from "../components/home/TestimonialsLToR";
 import Layout from "../hocs/Layout"
 import { useEffect } from "react";
 
@@ -18,8 +16,7 @@ const Home = () => {
         <Banner />
         <Promotions />
         <Feature />
-        <Testimonials />
-        <TestimonialsLToR />
+
         <Clients />
       </Layout>
    )
